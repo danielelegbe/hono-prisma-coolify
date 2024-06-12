fd1426873eb0e5c02a0f37a7312f30fb395ad879
Daniel Elegbe <daniel_elegbe@hotmail.co.uk>
2024-06-12 22:51:09 +0100

    Switch from bun to node

--- dist/index.js
+++ dist/index.js
@@ -2,13 +2,8 @@ import { Hono } from "hono";
 import { postsApp } from "routes/posts";
 import { logger } from "hono/logger";
 import { serve } from "@hono/node-server";
-
 const api = new Hono().basePath("/api");
-
 api.use(logger());
-
 api.route("/posts", postsApp);
-
 serve(api);
-
 export default api;
